ff1a505a958e06ad4d2920877b922a360417cd73
lsy <lsy@rrc.ca>
2025-01-19 16:03:27 -0600

    created jest config file, test for health checkup implemented

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
 import express, { Express } from "express";
+import setupSwagger from "../config/swagger";
 
 const app: Express = express();
 
+setupSwagger(app);
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+/**
+ * @openapi
+ * /tasks:
+ *  get:
+ *   summary: Retrieve a list of tasks
+ *   tags: [Tasks]
+ *   responses:
+ *    200:
+ *     description: A list of tasks
+ */
+
+app.get("/tasks", (req, res) => {
+    res.send("Retrieve tasks")
 });
 
 app.get("/api/v1/health", (req, res) => {
